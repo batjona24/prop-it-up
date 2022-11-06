8ba3a1ff1faae048ede43f4d8a1a53299500203c
batjona24 <88026430+batjona24@users.noreply.github.com>
2022-11-06 21:18:03 +0100

    Update PersonCard.js

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -4,7 +4,7 @@ const PersonCard = (props) =>{
     return(
         <div>
             <h1> { props.tittle }</h1>
-            <p>{ props.age }</p>
+            <p>Age : { props.age }</p>
             <p>{ props.desc }</p>
         </div>
 
